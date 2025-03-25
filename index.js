d33f3b0da6b89acf51dc61875d8376696a27c97c
zaharah <nalunkumazaharahbbosa@gmail.com>
2025-03-25 18:59:55 +0300

    created a javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+let stm1 = "extravaganza"
+let stm2 = (stm1.length)
+// console.log(stm2);
+let stm3 = stm1.slice(-4,);
+console.log(stm3);
+
+
+let a = "The quick fox jumped over the lazy dog"
+let b = a.replace("u","eat");
+console.log(b);
+
+
+
+
