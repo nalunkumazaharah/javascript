4cab98f1ffd5286c587cd3b7430e5fbe018f1d70
zaharah <nalunkumazaharahbbosa@gmail.com>
2025-03-28 08:52:08 +0300

    created strings

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-let stm1 = "extravaganza"
-let stm2 = (stm1.length)
-// console.log(stm2);
-let stm3 = stm1.slice(-4,);
-console.log(stm3);
+// let stm1 = "extravaganza"
+// let stm2 = (stm1.length)
+// // console.log(stm2);
+// let stm3 = stm1.slice(-4,);
+// console.log(stm3);
+
+// //Number 2
+// let a = "The quick fox jumped over the lazy dog"
+// let b = a.replace("u","eat");
+// console.log(b);
+
+// //Number 3
+// const story = "The quick brown fox jumped over the lazt dog"
+// let story1  = story.toLowerCase().split("the").length-1;
+// // let story2= story.split("the").length-1
+
+// console.log({story1})
+// let story4 =story.toLowerCase() .split("brown").lenght-1;
+// console.log()
 
 
-let a = "The quick fox jumped over the lazy dog"
-let b = a.replace("u","eat");
-console.log(b);
 
 
+
+
+// //number4
+// const string1 = "The pupils are reading in the library";
+// let stmt = string1.indexOf("are") ;
+// console.log(stmt);
+// const string2 = "The child was sitting on the table before it fell"
+// let indexofsitting = string2.indexOf("sitting")
+// console.log(indexofsitting);
+
+// //number six
+// let case1 = "wonderful"
+// let case2 = case1.toUpperCase();
+// console.log(case2)
+
+// let lower1 = "amazing"
+// let lower2 = lower1.toLowerCase();
+// console.log(lower2);
+
+let lower3 = "UndERneath"
+let lower4 = lower3.toLowerCase();
+console.log(lower4)
+
+let title = "A wonderful world".split(' ')
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+  .join(' ');
+console.log(title);
 
 
